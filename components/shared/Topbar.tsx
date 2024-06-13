68e8eb10dbaf2537fbc56ff61e109c165adad323
AishiD <97897398+AishiD@users.noreply.github.com>
2024-06-13 17:47:07 +0530

    leftside bar needs to be fixed

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,34 @@
+import Link from "next/link";
+import Image from "next/legacy/image";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+
 function Topbar() {
-    return <h1>Topbar</h1>
+    return(
+        <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-dark-2 px-6 py-3">
+            <Link href="/" className="flex items-center gap-4">
+            <Image src="/assets/logo.svg" alt="logo" width={25} height={28} />
+            <p className="text-heading3-bold text-light-1 max-xs:hidden">Stringz</p>
+            </Link>
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className="flex cursor-pointer">
+                                <Image src="/assets/logout.svg" alt="logout"
+                                width={24} height={24} />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+                <OrganizationSwitcher
+                appearance={{
+                    elements: {
+                        organisationSwitcherTrigger: "py-2 px-4"
+                    }
+                }} 
+                />
+            </div>         
+        </nav>
+    )
 }
 export default Topbar;
